0985fa3ab586e9a1a27c7094f8429bc890b28f44
LeJexy <jdejesus965@gmail.com>
2022-02-08 21:10:27 +0800

    check for the progress status

--- frontend/src/containers/Reports/styles.js
+++ frontend/src/containers/Reports/styles.js
@@ -1,3 +1,66 @@
 import styled from "styled-components";
 import { theme } from "@/theme";
 
+export const Wrapper = styled.div`
+    padding: 1.5em;
+    display: flex;
+    column-gap: .5em;
+`;
+
+export const LeftSide = styled.div`  
+    padding: .2em .5em;
+    overflow: scroll;   
+    height: 90vh;
+    flex-basis: 39%;
+    
+    
+`;
+
+export const RightSide = styled.div`
+    padding-top: 1em;
+    flex-basis: 70%;
+    display: flex;
+    flex-direction: column;
+    row-gap: 1em;
+`;
+
+export const DisplayNotif = styled.div`
+    padding: 3em 2.5em;
+    box-shadow: 0px 3px 10px 1px rgba(46, 54, 68, 0.2); 
+    border-radius: .5em;   
+    display: flex;
+    flex-direction: column;
+    row-gap: 1em;
+    
+`;
+
+export const To = styled.div`
+    font-family: ${theme.fonts.avenirBlack};
+    font-size: ${theme.fontSizes.xl};    
+`;
+
+export const SubText = styled.div`
+    font-family: ${theme.fonts.avenirBlack};
+    font-size: ${theme.fontSizes.md};
+`;
+
+export const MainMessage = styled.div`
+    font-family: ${theme.fonts.avenirBook};
+    font-size: ${theme.fontSizes.md};
+`;
+
+export const ProgressBar = styled.div`
+    padding: 3em 2em;
+    box-shadow: 0px 3px 10px 1px rgba(46, 54, 68, 0.2);  
+    border-radius: .5em;  
+`;
+
+export const TitleBar = styled.div`
+    font-family: ${theme.fonts.avenirBlack};
+    font-size: ${theme.fontSizes.lg};
+    color: ${theme.colors.default};
+    text-align: right;
+    width: 6em;
+  
+
+`;
