adddb0492d3cff0ca92e2008de4dabd10b3744f8
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2022-02-17 20:06:23 +0800

    updated routes and middlewares

---
+++ frontend/src/pages/payroll/[id]/index.jsx
@@ -0,0 +1,8 @@
+import View from "@/containers/Payroll/view";
+import React from "react";
+
+const PayrollViewpage = () => {
+  return <View />;
+};
+
+export default PayrollViewpage;
---
+++ frontend/src/pages/reports/index.jsx
@@ -0,0 +1,8 @@
+import Reports from "@/containers/Reports";
+import React from "react";
+
+const ReportsPage = () => {
+  return <Reports />;
+};
+
+export default ReportsPage;
