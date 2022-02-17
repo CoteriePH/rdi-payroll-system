95c9a456e57a2a58bbe9424ec28f329cf66a011d
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2022-02-17 20:54:50 +0800

    added next head

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -6,12 +6,20 @@ import { theme } from "@/theme";
 import { PersistGate } from "redux-persist/integration/react";
 import MainLayout from "@/layouts/MainLayout";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 function MyApp({ Component, pageProps: { session, ...pageProps }, router }) {
   return (
     <SessionProvider session={session} refetchInterval={5 * 60}>
       <ThemeProvider theme={theme}>
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
+            <Head>
+              <title>RDI Payroll System</title>
+              <meta
+                name="viewport"
+                content="initial-scale=1.0, width=device-width"
+              />
+            </Head>
             {router.pathname.includes("/auth") ? (
               <Component {...pageProps} />
             ) : (
