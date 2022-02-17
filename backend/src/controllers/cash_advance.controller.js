0549db755296e7d9c9d098f383f6340c0f41c06b
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2022-02-17 23:35:00 +0800

    added decline route

--- backend/src/controllers/cash_advance.controller.js
+++ backend/src/controllers/cash_advance.controller.js
@@ -136,28 +136,38 @@ exports.update = async (req, res) => {
   }
 };
 
-exports.delete = async (req, res) => {
+exports.decline = async (req, res) => {
   try {
-    const { cash_advance_eligibility } = req.body;
-    if (cash_advance_eligibility == "true" || cash_advance_eligibility == 1) {
-      const cash_advance = await CashAdvance.findByPk(req.params.id);
-      await Employee.update(
-        {
-          cash_advance_eligibility,
+    const cash_advance = await CashAdvance.findByPk(req.params.id);
+    await Employee.update(
+      {
+        cash_advance_eligibility: true,
+      },
+      {
+        where: {
+          id: cash_advance.employee_id,
         },
-        {
-          where: {
-            id: cash_advance.employee_id,
-          },
-        }
-      );
-    }
+      }
+    );
     await CashAdvance.destroy({
       where: {
         id: req.params.id,
       },
     });
-    return res.status(200).send("Cash advance deleted successfully");
+    return res.status(200).send("Cash advance declined");
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+};
+
+exports.delete = async (req, res) => {
+  try {
+    await CashAdvance.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    return res.status(200).send("Cash advance deleted successfully.");
   } catch (error) {
     return res.status(400).send(error.message);
   }
