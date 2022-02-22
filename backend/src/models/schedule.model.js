6cee32d35fc2a9e052131046573e0c37b067e533
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2022-02-22 22:47:48 +0800

    fixed sched table

--- backend/src/models/schedule.model.js
+++ backend/src/models/schedule.model.js
@@ -1,27 +1,30 @@
 module.exports = (sequelize, Sequelize, DataTypes) => {
-    const Schedule = sequelize.define(
-      "schedule", // Model name
-      {
-        // Attributes
-        id:{
-            type: DataTypes.UUID,
-            defaultValue: Sequelize.UUIDV4,
-            primaryKey: true,
-            allowNull: false,
-        },
-        schedule:{
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+  const Schedule = sequelize.define(
+    "schedule", // Model name
+    {
+      // Attributes
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
       },
-      {
-        timestamps: true,
-        underscrored: true,
-        createdAt: "created_at",
-        updatedAt: "updated_at",
-      }
-    );
-  
-    return Schedule;
-  };
-  
+      start_time: {
+        type: DataTypes.TIME,
+        allowNull: false,
+      },
+      end_time: {
+        type: DataTypes.TIME,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: true,
+      underscrored: true,
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    }
+  );
+
+  return Schedule;
+};
